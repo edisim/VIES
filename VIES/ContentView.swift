78efaecea41e228152f49e0a5c2a6ab9b18580e7
FranicevicNikola <nikola007f@gmail.com>
2020-12-17 07:21:48 +0100

    Initial Commit

---
+++ VIES/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VIES
+//
+//  Created by Master Family on 17/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VIES/VIESApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIESApp.swift
+//  VIES
+//
+//  Created by Master Family on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct VIESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
