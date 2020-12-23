eb35af07e00256a5dd462e24927a38c8d32a940e
FranicevicNikola <nikola007f@gmail.com>
2020-12-23 06:27:47 +0100

    inns

---
+++ VIES/Model.swift
@@ -0,0 +1,20 @@
+//
+//  Model.swift
+//  VIES
+//
+//  Created by Master Family on 18/12/2020.
+//
+
+import Foundation
+
+struct Response: Codable {
+    let valid: Bool
+    let vatNumber, name, address, countryCode: String
+
+    enum CodingKeys: String, CodingKey {
+        case valid
+        case vatNumber = "vat_number"
+        case name, address
+        case countryCode = "country_code"
+    }
+}
